a99efe789c9f38df72732cfc20a8835d2e45d83a
KamikadzeBull <kenny1634tlt@yandex.ru>
2019-01-28 14:24:20 +0300

    One more commit

--- src/HashTable.java
+++ src/HashTable.java
@@ -159,7 +159,6 @@ public class HashTable<K, V> implements Map<K, V> {
         int old = capacity;
         capacity *= 2;
         Node[] between = Arrays.copyOf(table, old);
-        boolean[] del = Arrays.copyOf(exists, old);
 
         table = new Node[capacity];
         exists = new boolean[capacity];
@@ -189,27 +188,146 @@ public class HashTable<K, V> implements Map<K, V> {
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-
+        for (Entry<? extends K, ? extends V> elem : m.entrySet()) {
+            put(elem.getKey(), elem.getValue());
+        }
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < capacity; i++) {
+            table[i] = null;
+            exists[i] = false;
+        }
+        size = 0;
     }
+
+    private <T> java.util.Iterator<T> getIterator(TypeIterator type) {
+        if (size == 0) {
+            return Collections.emptyIterator();
+        } else {
+            return new HashTableIterator<>(type);
+        }
+    }
+
+    enum TypeIterator {ENTRY, KEYS, VALUES}
 
     @Override
     public Set<K> keySet() {
-        return null;
+        return keySet;
+    }
+
+    private Set<K> keySet = new KeySet();
+
+    private class KeySet extends AbstractSet<K> {
+        public java.util.Iterator<K> iterator() {
+            return getIterator(TypeIterator.KEYS);
+        }
+        public int size() {
+            return size;
+        }
+        public boolean contains(Object o) {
+            return containsKey(o);
+        }
+        public boolean remove(Object o) {
+            return HashTable.this.remove(o) != null;
+        }
+        public void clear() {
+            HashTable.this.clear();
+        }
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return values;
+    }
+
+    private Collection<V> values = new ValueCollection();
+
+    private class ValueCollection extends AbstractCollection<V> {
+        public Iterator<V> iterator() {
+            return getIterator(TypeIterator.VALUES);
+        }
+        public int size() {
+            return size;
+        }
+        public boolean contains(Object o) {
+            return containsValue(o);
+        }
+        public void clear() {
+            HashTable.this.clear();
+        }
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        return entrySet;
+    }
+
+    private Set<Map.Entry<K, V>> entrySet = new EntrySet();
+
+    class EntrySet extends AbstractSet<Entry<K, V>> {
+        public Iterator<Entry<K, V>> iterator() {
+            return getIterator(TypeIterator.ENTRY);
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+        public boolean add(Map.Entry<K, V> o) {
+            return super.add(o);
+        }
+        public boolean contains(Object o) {
+            Object key = getK(o);
+            return HashTable.this.containsKey(key);
+        }
+        public boolean remove(Object o) {
+            Object key = getK(o);
+            return HashTable.this.remove(key) != null;
+        }
+        private Object getK(Object o){
+            if (!(o instanceof Map.Entry)) {
+                return false;
+            }
+            Map.Entry<?, ?> entry = (Map.Entry<?, ?>) o;
+            return entry.getKey();
+        }
+        public void clear() {
+            HashTable.this.clear();
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Map)) {
+            return false;
+        }
+        if (((Map) obj).size() != this.size){
+            return false;
+        }
+        for (Object o : ((Map) obj).entrySet()) {
+            @SuppressWarnings("unchecked")
+            Entry<K, V> elem = (Entry<K, V>) o;
+            K key = elem.getKey();
+            V value = elem.getValue();
+            if (!containsKey(key) || get(key) != value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for (Entry<K, V> entry : this.entrySet()) {
+            hash += entry.hashCode();
+        }
+        return hash;
     }
 
     enum Type {
@@ -262,6 +380,51 @@ public class HashTable<K, V> implements Map<K, V> {
             Map.Entry<?, ?> e = (Map.Entry<?, ?>) obj;
             return (key == null ? e.getKey() == null : key.equals(e.getKey())) &&
                     (value == null ? e.getValue() == null : value.equals(e.getValue()));
+        }
+    }
+
+    class HashTableIterator<T> implements java.util.Iterator<T> {
+
+        Node[] table = HashTable.this.table;
+        boolean[] exist = HashTable.this.exists;
+
+        int i = -1;
+        int count = 0;
+        int size1 = HashTable.this.size;
+        TypeIterator type;
+
+        HashTableIterator(TypeIterator type) {
+            this.type = type;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return count < size1;
+        }
+
+        @Override
+        public T next() {
+            i++;
+            while (i < capacity) {
+                if (exist[i]) {
+                    Map.Entry node = table[i];
+                    count++;
+                    switch (type) {
+                        case KEYS:
+                            //noinspection unchecked
+                            return (T) node.getKey();
+                        case VALUES:
+                            //noinspection unchecked
+                            return (T) node.getValue();
+                        default:
+                            //noinspection unchecked
+                            return (T) node;
+                    }
+                } else {
+                    i++;
+                }
+            }
+            return null;
         }
     }
 }
